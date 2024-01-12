ab310ecd6fe0585a157c4793564e1e4ec0d07960
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-12 18:37:09 +0100

    TL-10 User tábla + adatbázis ellenőrzés error üzenetek

--- backend/libs/database.js
+++ backend/libs/database.js
@@ -4,27 +4,34 @@ import logger from './logger.js'
 import { config } from 'dotenv'
 config()
 
+// Adatbázis ellenőrzés - ha nincs, akkor létrehozza
 const checkDatabase = async () => {
+  try {
     const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
     })
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\` DEFAULT CHARSET \`utf8\` COLLATE \`utf8_general_ci\`;`)
     logger.db('Database successfully created (if not exists).')
+  } catch {
+    logger.error('Something went wrong when trying to connect and create the database.')
+    logger.error('Please start the database if it is not already running.')
+  }
 }
 
+// Sequelize adatbázis kapcsolat
 const db = new Sequelize({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    dialect: 'mysql',
-    logging: (msg) => {
-        const id = msg.split(' ')[1].replace(':', '')
-        const sql = msg.split(': ')[1]
-        logger.db(`${sql} ${id}`)
-    }
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: 'mysql',
+  logging: (msg) => {
+    const id = msg.split(' ')[1].replace(':', '')
+    const sql = msg.split(': ')[1]
+    logger.db(`${sql} ${id}`)
+  }
 })
 
 export { db, checkDatabase }
--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,9 @@ const users = db.define('users', {
     username: { type: DataTypes.STRING(128), allowNull: false },
     email: { type: DataTypes.STRING(128), allowNull: false },
     password: { type: DataTypes.STRING(128), allowNull: false },
-    image: { type: DataTypes.STRING(256), allowNull: true }
+    avatar: { type: DataTypes.STRING(256), allowNull: true },
+    verifyToken: { type: DataTypes.STRING(64), allowNull: true },
+    registerDate: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW }
 }, { createdAt: false, updatedAt: false });
 
 export default users
