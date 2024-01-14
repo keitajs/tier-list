f01d3114a8321adf4dca87101ffb0c91e22115f7
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-14 19:39:50 +0100

    TL-18 Navbar hover effektek

--- frontend/src/components/List/Navbar.jsx
+++ frontend/src/components/List/Navbar.jsx
@@ -7,10 +7,10 @@ function Navbar(props) {
     <div className='absolute right-5 bottom-5 p-1.5 rounded-2xl bg-neutral-900 bg-opacity-85 z-50'>
       <div className='relative flex rounded-xl bg-neutral-800 bg-opacity-50'>
         <div className={`z-0 absolute h-16 w-16 rounded-xl bg-blue-500 ${props.editor ? 'left-0' : 'left-16'} transition-all`}></div>
-        <button onClick={() => props.setEditor(true)} className='z-10 flex items-center justify-center h-16 w-16 rounded-xl'>
+        <button onClick={() => props.setEditor(true)} className={`z-10 flex items-center justify-center h-16 w-16 rounded-xl ${props.editor ? '' : 'hover:bg-neutral-800 transition-colors'}`}>
           <FontAwesomeIcon icon={faPen} className='h-6' />
         </button>
-        <button onClick={() => props.setEditor(false)} className='z-10 flex items-center justify-center h-16 w-16 rounded-xl'>
+        <button onClick={() => props.setEditor(false)} className={`z-10 flex items-center justify-center h-16 w-16 rounded-xl ${props.editor ? 'hover:bg-neutral-800 transition-colors' : ''}`}>
           <FontAwesomeIcon icon={faGears} className='h-6' />
         </button>
       </div>
