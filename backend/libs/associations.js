9330a4821ade3b0c7aa0a0aa2c6026fe257facd7
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-14 18:57:28 +0100

    TL-10 Lista tábla hozzáadva

--- backend/libs/associations.js
+++ backend/libs/associations.js
@@ -1,8 +1,11 @@
 import { db, checkDatabase } from '../libs/database.js'
 import users from '../models/user.js'
+import lists from '../models/list.js'
 
 (async () => {
-    // adatbázis kapcsolatok ide jönnek
+    // Adatbázis kapcsolatok
+    users.hasMany(lists, { foreignKey: { name: 'userId', allowNull: false }, onDelete: 'CASCADE' })
+    lists.belongsTo(users)
 
     await checkDatabase()
     await db.sync()
---
+++ backend/models/list.js
@@ -0,0 +1,12 @@
+import { DataTypes } from "sequelize"
+import { db } from '../libs/database.js'
+
+const lists = db.define('lists', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING(128), allowNull: false },
+    description: { type: DataTypes.STRING(256), allowNull: true },
+    status: { type: DataTypes.INTEGER(1), allowNull: false, defaultValue: 1 },
+    private: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true }
+}, { createdAt: false, updatedAt: false });
+
+export default lists
