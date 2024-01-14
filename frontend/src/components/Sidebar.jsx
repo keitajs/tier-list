8bb125d0947216fc8010a5aca8c610a3c3f37784
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-14 18:59:18 +0100

    TL-25 Kijelentkezés + group/group-hover használata változóba mentett hover helyett

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react'
+import React from 'react'
+import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faHome, faUser, faList, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import NavItem from './Sidebar/NavItem'
 import ListItem from './Sidebar/ListItem'
 
-function Header() {
-  const [hover, setHover] = useState(false)
+function Sidebar(props) {
+  const Logout = () => {
+    axios.delete('http://localhost:2000/logout').then(() => props.history('/'))
+  }
 
   return (
-    <div className='absolute left-0 top-0 bottom-0 w-max rounded-e-3xl bg-neutral-800 overflow-hidden z-50'>
-      <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className={`relative ${hover ? 'w-60' : 'w-14'} h-full p-4 bg-neutral-900 bg-opacity-85 overflow-hidden whitespace-nowrap transition-all`}>
+    <div className='group absolute left-0 top-0 bottom-0 w-max rounded-e-3xl bg-neutral-800 shadow-lg overflow-hidden z-50'>
+      <div className='relative w-14 group-hover:w-60 h-full p-4 bg-neutral-900 bg-opacity-85 overflow-hidden whitespace-nowrap transition-all'>
         <div className='flex flex-col w-52 h-12 text-lg font-medium tracking-widest'>
           <div className='flex items-center gap-4 mb-3'>
             <FontAwesomeIcon icon={faBars} className='w-6' />
-            <div className={`${hover ? '' : 'opacity-0'} transition-opacity`}>TIER LIST</div>
+            <div className='opacity-0 group-hover:opacity-100 transition-opacity'>TIER LIST</div>
           </div>
-          <div className={`${hover ? 'w-52' : 'w-6'} h-0.5 bg-blue-500 transition-all`}></div>
+          <div className='w-6 group-hover:w-52 h-0.5 bg-blue-500 transition-all'></div>
         </div>
 
         <div className='top-16 flex flex-col gap-2 w-52 my-8'>
-          <NavItem text={'Főoldal'} icon={faHome} pathname={'/'} hover={hover} />
-          <NavItem text={'Profile'} icon={faUser} pathname={'/profile'} hover={hover} />
-          <NavItem text={'Listák'} icon={faList} pathname={'/list'} hover={hover} />
+          <NavItem text={'Főoldal'} icon={faHome} pathname={'/'} />
+          <NavItem text={'Profile'} icon={faUser} pathname={'/profile'} />
+          <NavItem text={'Listák'} icon={faList} pathname={'/list'} />
         </div>
 
-        <div className={`flex flex-col gap-0.5 w-52 py-2 rounded-xl bg-neutral-900 overflow-hidden ${hover ? '' : 'opacity-0 pointer-events-none'} transition-opacity`}>
+        <div className={`flex flex-col gap-0.5 w-52 py-2 rounded-xl bg-neutral-900 overflow-hidden opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity`}>
           <ListItem name={'List 1'} status={2} />
           <ListItem name={'List 2'} status={1} />
           <ListItem name={'List 3'} status={3} />
@@ -32,13 +35,13 @@ function Header() {
           <ListItem name={'List 5'} status={3} />
         </div>
 
-        <button className='absolute left-5 bottom-5 flex items-center text-red-500 hover:text-red-400 transition-all'>
+        <button onClick={Logout} className='absolute left-5 bottom-5 flex items-center text-red-500 hover:text-red-400 transition-all'>
           <FontAwesomeIcon icon={faRightFromBracket} className='mr-3' />
-          <div className={`text-xl ${hover ? '' : 'opacity-0'} transition-opacity`}>Kijelentkezés</div>
+          <div className='text-xl opacity-0 group-hover:opacity-100 transition-opacity'>Kijelentkezés</div>
         </button>
       </div>
     </div>
   )
 }
 
-export default Header
+export default Sidebar
--- frontend/src/components/Sidebar/NavItem.jsx
+++ frontend/src/components/Sidebar/NavItem.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function NavItem(props) {
-  const [hover, setHover] = useState(false)
-
   return (
-    <Link to={props.pathname} className='relative flex items-center gap-4 h-12 text-lg' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+    <Link to={props.pathname} className='group/navitem relative flex items-center gap-4 h-12 text-lg'>
       <FontAwesomeIcon icon={props.icon} className='w-6' />
-      <div className={`${props.hover ? '' : 'opacity-0'} transition-opacity`}>{props.text}</div>
-      {window.location.pathname === props.pathname ? <div className='absolute -left-4 h-2/3 w-1 rounded-e-lg bg-blue-500'></div> : <div className={`absolute ${hover ? '-left-4' : '-left-6'} h-2/3 w-1 rounded-e-lg bg-white transition-all`}></div>}
+      <div className={`opacity-0 group-hover:opacity-100 transition-opacity`}>{props.text}</div>
+      {window.location.pathname === props.pathname ? <div className='absolute -left-4 h-2/3 w-1 rounded-e-lg bg-blue-500'></div> : <div className={`absolute -left-6 group-hover/navitem:-left-4 h-2/3 w-1 rounded-e-lg bg-white transition-all`}></div>}
     </Link>
   )
 }
