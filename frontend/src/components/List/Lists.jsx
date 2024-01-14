92d752343d612e9bc13ab2ab0a3bc8928e3c6a5c
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-14 19:04:59 +0100

    TL-21 Lista kezelő oldal elkezdve

--- frontend/src/components/List/Lists.jsx
+++ frontend/src/components/List/Lists.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import ListItem from './Lists/ListItem'
 
-function Lists() {
+function Lists(props) {
+  const [lists, setLists] = useState([])
+  const [publicLists, setPublicLists] = useState([])
+  const [activeList, setActiveList] = useState(null)
+
+  const getUserLists = async () => {
+    const { data } = await axios.get('http://localhost:2000/user/lists')
+    setLists(data)
+  }
+
+  const getPublicLists = async () => {
+    const { data } = await axios.get('http://localhost:2000/lists/public')
+    setPublicLists(data)
+  }
+
+  useEffect(() => {
+    getUserLists().catch(() => props.history('/'))
+    getPublicLists().catch(err => {alert('Server error'); console.log(err)})
+  }, [props])
+
   return (
-    <div>Listáid...</div>
+    <div className='flex h-full gap-6'>
+      <div className='w-1/2 h-full rounded-3xl bg-neutral-900 bg-opacity-85'>
+        <div className='m-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>Listáid</div>
+        <div className='flex flex-col gap-2.5'>
+          {lists.map(list => <ListItem list={list} activeList={activeList} onClick={() => setActiveList(list)} />)}
+          <button onClick={() => setActiveList(null)} className='group flex items-center justify-center mx-7 mt-2 py-2.5 rounded-xl bg-neutral-950 bg-opacity-30 hover:bg-opacity-20 transition-all'>
+            <FontAwesomeIcon icon={faPlus} className='mx-2' />
+            <span className='text-xl w-0 max-w-max group-hover:w-32 text-nowrap overflow-hidden transition-[width]'>Új lista</span>
+          </button>
+        </div>
+      </div>
+      <div className='flex flex-col gap-6 w-1/2 h-full'>
+        <div className='h-1/2 rounded-3xl bg-neutral-900 bg-opacity-85'>
+          <div className='m-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>{activeList ? activeList.name : 'Új lista'}</div>
+        </div>
+        <div className='h-1/2 rounded-3xl bg-neutral-900 bg-opacity-85'>
+          {activeList ? <>
+            <div className='m-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>Jogosultságok</div>
+          </> : <>
+            <div className='m-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>Publikus listák</div>
+            <div className='flex flex-col gap-2.5'>
+              {publicLists.map(list => <ListItem list={list} public={true} />)}
+            </div>
+          </>}
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/components/List/Lists/ListItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faEllipsis, faDumpsterFire, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+function ListItem(props) {
+  return (
+    <button key={props.list.id} onClick={props.onClick} className='flex items-center justify-between mx-5 px-4 py-3 rounded-xl bg-neutral-950 even:bg-opacity-70 odd:bg-opacity-85 hover:bg-neutral-950 hover:bg-opacity-40 transition-all'>
+      <div className='flex items-center text-xl'>
+        <span className='mr-3'>{props.list.name}</span>
+        {[<FontAwesomeIcon icon={faEllipsis} className='text-white' />, <FontAwesomeIcon icon={faCheck} className='text-emerald-500' />, <FontAwesomeIcon icon={faDumpsterFire} className='text-red-500' />][props.list.status - 1]}
+      </div>
+      <div className='flex items-center text-sm'>
+        {props.public ? <p>
+          <span className='text-blue-500 opacity-75'>Admin</span><span className='opacity-40'> frissítette ekkor: </span><span className='text-blue-500 opacity-75'>2024.01.12.</span>
+        </p> : <>
+          <span className='opacity-40'>{props.list.private ? 'Private' : 'Public'}</span>
+          <FontAwesomeIcon icon={faChevronRight} className={`${props.list.id === props.activeList?.id ? 'ml-2 w-3' : 'ml-0 w-0'} transition-all`} />
+        </>}
+      </div>
+    </button>
+  )
+}
+
+export default ListItem
