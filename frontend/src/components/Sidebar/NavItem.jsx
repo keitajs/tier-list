112aaafb87362a31c7254d6dbb43bfd7129ffb32
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-15 18:08:21 +0100

    TL-25 Sidebar NavItem a fő routeot figyeli (/list/editor-ból /list-et nézi csak) -> Nem jelzi, hogy az oldalon van bug javítva

--- frontend/src/components/Sidebar/NavItem.jsx
+++ frontend/src/components/Sidebar/NavItem.jsx
@@ -7,7 +7,7 @@ function NavItem(props) {
     <Link to={props.pathname} className='group/navitem relative flex items-center gap-4 h-12 text-lg'>
       <FontAwesomeIcon icon={props.icon} className='w-6' />
       <div className={`opacity-0 group-hover:opacity-100 transition-opacity`}>{props.text}</div>
-      {window.location.pathname === props.pathname ? <div className='absolute -left-4 h-2/3 w-1 rounded-e-lg bg-blue-500'></div> : <div className={`absolute -left-6 group-hover/navitem:-left-4 h-2/3 w-1 rounded-e-lg bg-white transition-all`}></div>}
+      {window.location.pathname.slice(0, window.location.pathname.split('/', 2).join('/').length) === props.pathname ? <div className='absolute -left-4 h-2/3 w-1 rounded-e-lg bg-blue-500'></div> : <div className={`absolute -left-6 group-hover/navitem:-left-4 h-2/3 w-1 rounded-e-lg bg-white transition-all`}></div>}
     </Link>
   )
 }
