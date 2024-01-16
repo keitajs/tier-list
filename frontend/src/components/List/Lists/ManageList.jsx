857ca53797f6d6cf72d703820c9ec59774b2224f
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-16 19:25:52 +0100

    TL-21 Lista hozzáadás, törlés, módosítás + Megnyitás button + Már kiválasztott listára kattintva bezárja azt

--- frontend/src/components/List/Lists/ManageList.jsx
+++ frontend/src/components/List/Lists/ManageList.jsx
@@ -1,12 +1,47 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faEllipsis, faDumpsterFire, faEye, faEyeSlash, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faEllipsis, faDumpsterFire, faEye, faEyeSlash, faXmark, faUpRightFromSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function ManageList(props) {
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [status, setStatus] = useState(1)
   const [visible, setVisible] = useState(false)
+
+  const createList = async () => {
+    try {
+      const { data } = await axios.post('http://localhost:2000/lists/create', { name, description, status, visible })
+      props.setLists(lists => { return [...lists, data] })
+      props.setActiveList(data)
+    } catch (err) { alert('Server error'); console.log(err) }
+  }
+
+  const updateList = async () => {
+    try {
+      await axios.patch(`http://localhost:2000/lists/update/${props.activeList.id}`, { name, description, status, visible })
+      props.setLists(lists => {
+        const list = lists.find(list => list.id === props.activeList.id)
+        list.name = name
+        list.description = description
+        list.status = status
+        list.private = !visible
+        props.setActiveList({ ...list })
+        return lists
+      })
+    } catch (err) { alert('Server error'); console.log(err) }
+  }
+
+  const removeList = async () => {
+    try {
+      await axios.delete(`http://localhost:2000/lists/remove/${props.activeList.id}`)
+      props.setLists(lists => {
+        lists.splice(lists.findIndex(list => list.id === props.activeList.id), 1)
+        return lists
+      })
+      props.setActiveList(null)
+    } catch (err) { alert('Server error'); console.log(err) }
+  }
 
   useEffect(() => {
     if (props.activeList) {
@@ -24,11 +59,17 @@ function ManageList(props) {
 
   return (
     <>
-      <div className='mb-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>
+      <div className='flex justify-between mb-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>
         <div className='relative flex items-center w-max'>
           <input type='text' name="name" id="name" value={name} onChange={e => setName(e.target.value)} placeholder='Lista neve' className='px-2 -mx-2 rounded-lg placeholder:text-neutral-500 bg-neutral-700/50 outline-none resize-none' />
           {!name ? <div className='absolute top-1/2 right-0 -translate-y-1/2 flex items-center'><FontAwesomeIcon icon={faXmark} className='text-rose-500 h-5 input-error-anim' /></div> : <></>}
         </div>
+        {props.activeList ? 
+        <div className='flex items-center gap-3 text-sm'>
+          <button className='flex items-center gap-2 opacity-75 hover:opacity-90 transition-opacity'>Megnyitás<FontAwesomeIcon icon={faUpRightFromSquare} /></button>
+          <button onClick={removeList} className='flex items-center gap-2 group text-rose-600 hover:text-rose-500 ml-0 hover:ml-2 transition-all'><div className='w-0 group-hover:w-32 max-w-max overflow-hidden transition-all'>Törlés</div><FontAwesomeIcon icon={faTrash} /></button>
+        </div>
+        : <></>}
       </div>
 
       <label className='mb-0.5 ml-1.5' htmlFor="description">Leírás</label>
@@ -48,7 +89,7 @@ function ManageList(props) {
       </div>
 
       <div className='flex gap-2 justify-center mt-2'>
-        <button className='w-1/5 py-0.5 rounded-lg bg-emerald-600 hover:bg-emerald-500 transition-colors'>{props.activeList ? 'Mentés' : 'Létrehozás'}</button>
+        <button onClick={() => props.activeList ? updateList() : createList()} className='w-1/5 py-0.5 rounded-lg bg-emerald-600 hover:bg-emerald-500 transition-colors'>{props.activeList ? 'Mentés' : 'Létrehozás'}</button>
         {props.activeList ? <button onClick={() => props.setActiveList(null)} className='w-1/5 py-0.5 rounded-lg bg-rose-600 hover:bg-rose-500 transition-colors'>Mégsem</button> : <></>}
       </div>
     </>
--- frontend/src/pages/List/Lists.jsx
+++ frontend/src/pages/List/Lists.jsx
@@ -25,7 +25,7 @@ function Lists(props) {
       <div className='p-5 xl:w-1/2 w-full xl:h-full h-max rounded-3xl bg-neutral-900/85'>
         <div className='mb-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>Listáid</div>
         <div className='flex flex-col gap-2.5'>
-          {lists.map(list => <ListItem key={list.id} list={list} activeList={activeList} onClick={() => setActiveList(list)} />)}
+          {lists.map(list => <ListItem key={list.id} list={list} activeList={activeList} onClick={() => activeList?.id === list.id ? setActiveList(null) : setActiveList(list)} />)}
           <button onClick={() => setActiveList(null)} className='group flex items-center justify-center mx-2 mt-2 py-2.5 rounded-xl bg-neutral-950/30 hover:bg-neutral-950/20 transition-all'>
             <FontAwesomeIcon icon={faPlus} className='mx-2' />
             <span className='text-xl w-0 max-w-max group-hover:w-32 text-nowrap whitespace-nowrap overflow-hidden transition-[width]'>Új lista</span>
@@ -34,7 +34,7 @@ function Lists(props) {
       </div>
       <div className='flex flex-col flex-1 gap-6 xl:w-1/2 w-full h-full max-h-full'>
         <div className='flex flex-col h-max p-5 rounded-3xl bg-neutral-900/85'>
-          <ManageList activeList={activeList} setActiveList={setActiveList} />
+          <ManageList setLists={setLists} activeList={activeList} setActiveList={setActiveList} />
         </div>
         <div className='flex flex-col flex-nowrap grow gap-5 min-h-0 p-5 rounded-3xl bg-neutral-900/85'>
           {activeList ? <ManagePermission /> : <PublicLists />}
