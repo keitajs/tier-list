6aca26a12e26dc37653dd7904a6f56f974fcb022
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-30 18:43:31 +0100

    TL-10 Kategória string hosszok módosítva

--- backend/models/category.js
+++ backend/models/category.js
@@ -3,9 +3,9 @@ import { db } from '../libs/database.js'
 
 const categories = db.define('categories', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: DataTypes.STRING(128), allowNull: true },
+  name: { type: DataTypes.STRING(32), allowNull: true },
   position: { type: DataTypes.INTEGER, allowNull: true },
-  color: { type: DataTypes.STRING(64), allowNull: true, defaultValue: '#ffffff' }
+  color: { type: DataTypes.STRING(32), allowNull: true, defaultValue: '#ffffff' }
 }, { createdAt: false, updatedAt: false });
 
 export default categories
