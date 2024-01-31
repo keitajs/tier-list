207f8407352ea6e418fbf7cc662423a5eab832ce
keitajs <126980311+keitajs@users.noreply.github.com>
2024-01-31 17:23:45 +0100

    TL-10 Permission táblából id kiszedve

--- backend/models/permission.js
+++ backend/models/permission.js
@@ -2,7 +2,6 @@ import { DataTypes } from "sequelize"
 import { db } from '../libs/database.js'
 
 const permissions = db.define('permissions', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   value: { type: DataTypes.INTEGER(1), allowNull: false, defaultValue: 1 }
 }, { createdAt: false, updatedAt: false });
 
