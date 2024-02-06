8a79c16d5af5454008fc81e1d3be8b88d9717bf7
keitajs <126980311+keitajs@users.noreply.github.com>
2024-02-06 18:22:46 +0100

    TL-19 Profil oldal elkezdve

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-
+import React, { useEffect, useMemo, useState } from 'react'
+import axios from 'axios'
 import Sidebar from '../components/Sidebar'
+import UserData from '../components/Profile/UserData'
+import Activity from '../components/Profile/Activity'
+import Characters from '../components/Profile/Characters'
+import Lists from '../components/Profile/Lists'
 
 function Profile(props) {
+  const [user, setUser] = useState({})
+  const [list, setList] = useState({})
+  const [weeklyActivies, setWeeklyActivies] = useState([])
+  const maxActivity = useMemo(() => Math.max(...weeklyActivies.map((activity) => activity.count)), [weeklyActivies])
+
+  const getUserData = async () => {
+    const { data } = await axios.get('http://localhost:2000/user/data')
+    setUser(data.user)
+    setWeeklyActivies(data.weeklyActivies)
+    setList(data.list)
+  }
+  
+  useEffect(() => {
+    getUserData().catch(err => props.history('/'))
+  }, [props])
+
   return (
-    <div>
+    <div className='min-h-screen p-6'>
       <Sidebar history={props.history} />
+
+      <div className='h-full ml-14'>
+        <div className='flex flex-col gap-6 fixed right-6 top-6 bottom-6 w-1/5'>
+          <div className='group relative aspect-square rounded-3xl bg-neutral-900/85 overflow-hidden'>
+            <img src={"https://avatars.githubusercontent.com/u/126980311?v=4"} alt="" className='w-full h-full object-cover' />
+            <div className='cursor-pointer absolute bottom-0 left-0 right-0 py-3 text-lg text-center bg-neutral-950 opacity-0 group-hover:opacity-35 hover:!opacity-90 transition-opacity'>Profilkép módosítás</div>
+          </div>
+          <div className='px-4 grow rounded-3xl bg-neutral-900/85'>
+            <UserData user={user} list={list} history={props.history} />
+          </div>
+        </div>
+        <div className='flex flex-col gap-3 w-4/5 pr-12'>
+          <Activity weeklyActivies={weeklyActivies} maxActivity={maxActivity} />
+          <Characters />
+          <Lists />
+        </div>
+      </div>
     </div>
   )
 }
