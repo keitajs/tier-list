5b0eb982a4f1f6f7b0e3fca5e52f90bf8ba6976f
keitajs <126980311+keitajs@users.noreply.github.com>
2024-02-12 18:25:26 +0100

    TL-35 Kép scroll javítva, +/- zoom gombok

--- frontend/src/components/Profile/Characters.jsx
+++ frontend/src/components/Profile/Characters.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUpRightFromSquare, faClose } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUpRightFromSquare, faPlus, faMinus, faClose } from '@fortawesome/free-solid-svg-icons'
 
 function Characters(props) {
   const [full, setFull] = useState(null)
   const [zoom, setZoom] = useState(0.5)
   const maxCount = useMemo(() => Math.max(...props.characters.map((character) => character.count)), [props.characters])
 
+  const zoomIn = () => setZoom(zoom => zoom + 0.05 <= 1.25 ? zoom + 0.05 : 1.25)
+  const zoomOut = () => setZoom(zoom => zoom - 0.05 >= 0.25 ? zoom - 0.05 : 0.25)
+
   const zoomScroll = (e) => {
-    if (e.deltaY < 0) return setZoom(zoom => zoom + 0.05 <= 1.25 ? zoom + 0.05 : 1.25)
-    setZoom(zoom => zoom - 0.05 >= 0.25 ? zoom - 0.05 : 0.25)
+    if (e.deltaY < 0) return zoomIn()
+    zoomOut()
   }
 
   const zoomClick = (e) => {
@@ -21,9 +24,18 @@ function Characters(props) {
     })
   }
 
+  const preventScroll = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    return false
+  }
+
   useEffect(() => {
-    setZoom(0.5)
-  }, [full])
+    if (!full) return setZoom(0.5)
+
+    window.addEventListener('wheel', preventScroll, { passive: false })
+    return () => window.removeEventListener('wheel', preventScroll, { passive: false })
+  }, [full])  
 
   return (
     <div className='p-5 rounded-3xl bg-neutral-900/85'>
@@ -32,13 +44,14 @@ function Characters(props) {
         {props.characters.map(character =>
           <div key={character.id} className='flex'>
             <div onClick={() => setFull(null)} onWheel={zoomScroll} className={`z-40 fixed inset-0 flex items-center justify-center bg-neutral-950/60 ${full === character.id ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} transition-opacity`}>
-              <div className='absolute inset-0 flex items-center justify-center overflow-y-scroll opacity-0 bg-red-500' onWheel={(e) => e.target.scrollTo(0, window.innerHeight / 2)} onScroll={(e) => e.target.scrollTo(0, window.innerHeight / 2)}>
-                <div onClick={zoomClick} className={`-mr-4 h-[200%] ${zoom > 0.5 ? 'cursor-zoom-out' : 'cursor-zoom-in'} bg-blue-500`} style={{width: window.innerHeight/4*3 * zoom}}></div>
-              </div>
-              <div style={{height: `${zoom*100}%`}} className={`pointer-events-none aspect-[3/4] rounded-2xl overflow-hidden transition-all ${full === character.id ? 'scale-100' : 'scale-0'}`}>
+              <div style={{height: `${zoom*100}%`}} onClick={zoomClick} className={`${zoom > 0.5 ? 'cursor-zoom-out' : 'cursor-zoom-in'} aspect-[3/4] rounded-2xl overflow-hidden transition-all ${full === character.id ? 'scale-100' : 'scale-0'}`}>
                 <img src={character.image.startsWith('http') ? character.image : `http://localhost:2000/characters/images/${character.image}`} alt="" className='w-full h-full object-cover' />
               </div>
-              <button onClick={() => setFull(null)} className='absolute top-4 right-6 opacity-25 hover:opacity-100 transition-opacity'><FontAwesomeIcon icon={faClose} className='h-8' /></button>
+              <button onClick={() => setFull(null)} className='absolute top-4 right-5 opacity-25 hover:opacity-100 transition-opacity'><FontAwesomeIcon icon={faClose} className='h-8' /></button>
+              <div className='absolute bottom-4 right-5 flex flex-col rounded-lg bg-neutral-700/50'>
+                <button onClick={e => {e.stopPropagation(); zoomIn()}} className='w-10 h-10 rounded-lg hover:bg-neutral-700 transition-all'><FontAwesomeIcon icon={faPlus} /></button>
+                <button onClick={e => {e.stopPropagation(); zoomOut()}} className='w-10 h-10 rounded-lg hover:bg-neutral-700 transition-all'><FontAwesomeIcon icon={faMinus} /></button>
+              </div>
             </div>
 
             <button onClick={() => setFull(character.id)} className='h-32 aspect-[3/4] rounded-2xl overflow-hidden'>
