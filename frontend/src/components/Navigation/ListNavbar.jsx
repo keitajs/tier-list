c966ac37ac85a26a760f9b97093ee60ce873a733
keitajs <126980311+keitajs@users.noreply.github.com>
2024-02-26 09:46:43 +0100

    TL-25 Lista Navbar nem jelenik meg, ha nincs megnyitva lista

---
+++ frontend/src/components/Navigation/ListNavbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen, faGears } from '@fortawesome/free-solid-svg-icons'
+
+function ListNavbar(props) {
+  return (
+    <div className={`fixed right-5 bottom-5 p-1.5 rounded-2xl bg-neutral-900/85 z-50 hidden sm:inline ${props.selectedList ? 'sm:inline' : 'sm:!hidden'}`}>
+      <div className='relative flex rounded-xl bg-neutral-800/50'>
+        <div className={`z-0 absolute h-16 w-16 rounded-xl bg-blue-500 ${window.location.pathname === '/list/editor' ? 'left-0' : 'left-16'} transition-all`}></div>
+        <Link to={'/list/editor'} className={`z-10 flex items-center justify-center h-16 w-16 rounded-xl ${window.location.pathname === '/list/editor' ? '' : 'hover:bg-neutral-800 transition-colors'}`}>
+          <FontAwesomeIcon icon={faPen} className='h-6' />
+        </Link>
+        <Link to={'/list'} className={`z-10 flex items-center justify-center h-16 w-16 rounded-xl ${window.location.pathname === '/list/editor' ? 'hover:bg-neutral-800 transition-colors' : ''}`}>
+          <FontAwesomeIcon icon={faGears} className='h-6' />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default ListNavbar
