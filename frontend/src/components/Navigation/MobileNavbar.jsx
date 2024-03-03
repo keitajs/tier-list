0e2e474a01374bce0bb53bca3577478b14cb22e3
keitajs <126980311+keitajs@users.noreply.github.com>
2024-03-03 17:04:08 +0100

    TL-25 Telefonos navbar kisebb z-index -> használható karakter képnézegető

--- frontend/src/components/Navigation/MobileNavbar.jsx
+++ frontend/src/components/Navigation/MobileNavbar.jsx
@@ -4,7 +4,7 @@ import NavItem from './MobileNavbar/NavItem'
 
 function MobileNavbar(props) {
   return (
-    <div className='fixed left-0 bottom-0 right-0 flex sm:hidden justify-between px-4 rounded-t-3xl shadow-lg overflow-hidden z-50 bg-neutral-900'>
+    <div className='fixed left-0 bottom-0 right-0 flex sm:hidden justify-between px-4 rounded-t-3xl shadow-lg overflow-hidden z-30 bg-neutral-900'>
       <NavItem text={'Főoldal'} icon={faHome} pathname={'/'} />
       <NavItem text={'Profil'} icon={faUser} pathname={'/profile'} />
       {props.selectedList ? <NavItem text={'Editor'} icon={faPen} pathname={'/list/editor'} /> : <></>}
