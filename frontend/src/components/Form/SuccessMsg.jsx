97af0ba8c8f0bc30a5d58a710f2b73dc916b5c3a
keitajs <126980311+keitajs@users.noreply.github.com>
2024-04-03 16:26:49 +0200

    TL-22 TL-23 Sikeres bejelentkezés / regisztráció háttérszín cserélve

--- frontend/src/components/Form/SuccessMsg.jsx
+++ frontend/src/components/Form/SuccessMsg.jsx
@@ -4,13 +4,17 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 function SuccessMsg(props) {
   return (
-    <div className='absolute inset-0 flex flex-col items-center justify-center rounded-3xl bg-emerald-600 success-box-anim'>
-      <div className='text-center w-full py-2 bg-emerald-600 z-10'>
-        <FontAwesomeIcon icon={faCheck} className='h-8 success-check-anim' />
-      </div>
-      <div className='h-14 flex flex-col items-center overflow-hidden'>
-        <span className='text-2xl success-msg-anim'>{props.msg}</span>
-        <span className='text-sm success-msg2-anim'>{props.text}</span>
+    <div className='absolute inset-0 rounded-3xl bg-neutral-800 overflow-hidden success-box-anim'>
+      <div className='flex flex-col items-center justify-center w-full h-full bg-neutral-900/50'>
+        <div className='text-center w-full bg-neutral-800 z-10'>
+          <div className='w-full h-full py-2 bg-neutral-900/50'>
+            <FontAwesomeIcon icon={faCheck} className='h-8 success-check-anim' />
+          </div>
+        </div>
+        <div className='h-14 flex flex-col items-center overflow-hidden'>
+          <span className='text-2xl success-msg-anim'>{props.msg}</span>
+          <span className='text-sm success-msg2-anim'>{props.text}</span>
+        </div>
       </div>
     </div>
   )
