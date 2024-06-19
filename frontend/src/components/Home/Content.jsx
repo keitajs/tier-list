6980161906b08f736c63f60df40e77604551a43b
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-19 16:52:25 +0200

    Főoldal Content rendszer egyszerűsítve

---
+++ frontend/src/components/Home/Content.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../ui/Button'
+
+export function Content({ title, paragraphs, link, center, left, right, logged, nextPage, setActive }) {
+  const ref = useRef(null)
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    if (!center) return
+
+    const t = ref.current.textContent.length * 50
+    const timeout = setTimeout(nextPage, t)
+    setTime(t)
+    return () => clearTimeout(timeout)
+  }, [center])
+
+  return (
+    <div ref={ref} className={`absolute flex flex-col mx-6 ${left ? 'translate-x-[60%]' : right ? '-translate-x-[60%]' : ''} transition-all ${!center ? 'scale-50 opacity-0 pointer-events-none select-none' : ''}`}>
+      <h2
+        className={`relative text-xl lg:text-3xl font-semibold w-max mb-5 pb-1 after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:rounded-lg after:bg-blue-500 ${center ? 'home-content-anim' : ''}`}
+        style={{"--time": `${time}ms`}}
+      >
+        {title}
+      </h2>
+
+      {paragraphs.map((p, i) => <p key={i} className='my-1 leading-5 text-base lg:text-lg'>{p}</p>)}
+
+      {logged ?
+        <Button href={link.href} target={link.target} width='max' className='mt-4 mb-1 ml-auto text-base lg:text-lg'>{link.text}</Button>
+      : <>
+        <Button onClick={() => setActive(2)} width='max' className='mt-4 mb-1 ml-auto text-base lg:text-lg'>Regisztráció</Button>
+        <button onClick={() => setActive(1)} className='ml-auto text-xs lg:text-sm opacity-60 hover:opacity-75 transition-opacity'>Már van felhasználód? Jelentkezz be itt.</button>
+      </>}
+    </div>
+  )
+}
+
+export const Contents = [
+  {
+    title: <>Lista készítés egyszerűen!</>,
+    paragraphs: [
+      <>Az oldal sokféle lehetőséget nyújt a gyors és egyszerű Tier List készítéshez!</>,
+      <>A <Link to={'https://myanimelist.net'} target='_blank' className='text-blue-200 hover:text-blue-100 transition-colors'>MyAnimeList</Link> oldalán található összes karaktert azonnal elérheted és felhasználhatod a listádban, csak a név szükséges a beazonosításához!</>
+    ],
+    link: {
+      href: '/list',
+      text: 'Listák'
+    }
+  },
+  {
+    title: <>Készíts listát barátaiddal!</>,
+    paragraphs: [
+      <>A listák beállításaiban jogosultságokat adhatsz barátaidnak, amivel engedélyezheted számukra a megtekintését vagy akár a közös szerkesztését is!</>,
+      <>Azonnal láthatod a mások által végrehajtott újításokat, ezzel hatékonyabbá téve a csapatmunkát!</>
+    ],
+    link: {
+      href: '/list',
+      text: 'Listák'
+    }
+  },
+  {
+    title: <>MyAnimeList API</>,
+    paragraphs: [
+      <>Egyszerűen és gyorsan használhatod a MyAnimeList által biztosított adatokat, ezáltal nem kell foglalkozni az adatok, képek keresésével, mivel azonnal elérhetőek!</>,
+      <>Az oldal a Jikan API-t használja az adatok lekérésére, ez egy bárki számára elérhető, Unofficial MyAnimeList API. További információkért kattints a gombra!</>
+    ],
+    link: {
+      href: 'https://jikan.moe',
+      text: 'Jikan API',
+      target: '_blank'
+    }
+  },
+  {
+    title: <>Profil statisztikák</>,
+    paragraphs: [
+      <>A profil oldalon láthatod a heti aktivitásodat, a legtöbbet használt karakterjeidet és listáidat!</>,
+      <>Készíts listákat, hogy még szebb grafikonok készülhessenek!</>
+    ],
+    link: {
+      href: '/profile',
+      text: 'Profil'
+    }
+  }
+]
--- frontend/src/components/Home/Index.jsx
+++ frontend/src/components/Home/Index.jsx
@@ -1,36 +1,29 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { Content, Contents } from './Content'
 
-import ContentA from './Contents/ContentA'
-import ContentB from './Contents/ContentB'
-import ContentC from './Contents/ContentC'
-import ContentD from './Contents/ContentD'
-
-function Home(props) {
+export default function Home({ active, logged, setActive }) {
   const [contentIds, setContentIds] = useState([0, 1, 2])
-  const [contents] = useState([ContentA, ContentB, ContentC, ContentD])
-  
+
   // Contentek közötti lapozás
   const nextPage = () => setContentIds([cIndex(contentIds[0] + 1), cIndex(contentIds[1] + 1), cIndex(contentIds[2] + 1)])
   const previousPage = () => setContentIds([cIndex(contentIds[0] - 1), cIndex(contentIds[1] - 1), cIndex(contentIds[2] - 1)])
   
   // Ha az index - 1 vagy index + 1 egy nem létező content id-t kap, akkor visszaadja a sor másik végén lévőt
   const cIndex = (index) => {
-    return index < 0 ? contents.length - 1 : index === contents.length ? 0 : index
+    return index < 0 ? Contents.length - 1 : index === Contents.length ? 0 : index
   }
 
   return (
-    <div className={`absolute flex w-full mr-0 lg:mr-12 ${props.active ? '' : 'opacity-0 pointer-events-none select-none translate-y-4'} transition-all`}>
+    <div className={`absolute flex w-full mr-0 lg:mr-12 ${active ? '' : 'opacity-0 pointer-events-none select-none translate-y-4'} transition-all`}>
       <div className='flex flex-col justify-center select-none'><FontAwesomeIcon icon={faChevronLeft} onClick={previousPage} className='cursor-pointer h-8 p-4 opacity-75 hover:opacity-100 transition-opacity' /></div>
       
       <div className='relative w-full h-max lg:h-screen flex items-center justify-center'>
-        { contents.map((Content, i) => <Content key={i} logged={props.logged} history={props.history} left={i === contentIds[0]} center={i === contentIds[1]} right={i === contentIds[2]} nextPage={nextPage} setActive={props.setActive} />) }
+        { Contents.map((content, i) => <Content key={i} {...content} left={i === contentIds[0]} center={i === contentIds[1]} right={i === contentIds[2]} logged={logged} nextPage={nextPage} setActive={setActive} />) }
       </div>
       
       <div className='flex flex-col justify-center select-none'><FontAwesomeIcon icon={faChevronRight} onClick={nextPage} className='cursor-pointer h-8 p-4 opacity-75 hover:opacity-100 transition-opacity' /></div>
     </div>
   )
 }
-
-export default Home
