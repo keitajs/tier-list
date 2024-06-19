f9d8a77942d2a722cf752bcfcc3f0d6a631f6e99
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-19 16:51:19 +0200

    Button: ha van href akkor Linkként működik

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,4 +1,6 @@
-export default function Button({ children, width, text, color, hover, onClick, className, disabled }) {
+import { Link } from "react-router-dom"
+
+export default function Button({ children, width, text, color, hover, href, target, onClick, className, disabled }) {
   if (!color || color === 'primary') {
     color = 'bg-blue-500'
     hover = 'hover:bg-blue-400'
@@ -14,22 +16,17 @@ export default function Button({ children, width, text, color, hover, onClick, c
     hover = 'hover:bg-rose-500'
   }
 
-  return (
-    <button
-      className={`
-        ${width ? `w-${width}` : 'w-full'}
-        ${width === 'max' || width === 'min' ? 'px-8' : 'px-0'}
-        py-1.5
-        ${text ? `text-${text}` : 'text-base'}
-        rounded-lg
-        ${color || 'bg-blue-500'}
-        ${disabled ? 'cursor-not-allowed' : hover || 'hover:bg-blue-400'}
-        transition-colors
-        ${className}
-      `}
-      onClick={onClick}
-    >
-        {children}
-    </button>
-  )
+  const btnClass = `
+    ${width ? `w-${width}` : 'w-full'}
+    ${width === 'max' || width === 'min' ? 'px-8' : 'px-0'}
+    py-1.5
+    ${text ? `text-${text}` : 'text-base'}
+    rounded-lg
+    ${color || 'bg-blue-500'}
+    ${disabled ? 'cursor-not-allowed' : hover || 'hover:bg-blue-400'}
+    transition-colors
+    ${className}
+  `
+
+  return href ? (<Link to={href} target={target} className={btnClass} onClick={onClick}>{children}</Link>) : (<button className={btnClass} onClick={onClick}>{children}</button>)
 }
