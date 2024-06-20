4a2c9623a3890dc709957fba6960bab575c1fc85
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-20 12:27:36 +0200

    Login mezők ürítése megnyitáskor

--- frontend/src/components/Home/Login.jsx
+++ frontend/src/components/Home/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { login } from '../../user'
 import Username from '../ui/Username'
@@ -6,7 +6,7 @@ import Password from '../ui/Password'
 import Button from '../ui/Button'
 import SuccessMsg from './SuccessMsg'
 
-export default function Login(props) {
+export default function Login({ history, active, setActive }) {
   const [searchParams] = useSearchParams()
   const [msg, setMsg] = useState('')
   const [text, setText] = useState('')
@@ -32,20 +32,27 @@ export default function Login(props) {
 
     setMsg(data.message)
     setText(`Másodperceken belül átirányítunk ${redirectURI ? 'az előző oldalra' : 'a listáidhoz'}`)
-    setTimeout(() => props.history(redirectURI ? redirectURI : '/list'), 2500)
+    setTimeout(() => history(redirectURI ? redirectURI : '/list'), 2500)
   }
 
+  useEffect(() => {
+    if (active) {
+      setUsername('')
+      setPassword('')
+    }
+  }, [active])
+
   return (
-    <div className={`absolute flex flex-col items-center mr-0 lg:mr-12 px-12 py-12 lg:py-16 rounded-3xl bg-neutral-900/50 ${props.active ? '' : 'opacity-0 pointer-events-none select-none translate-y-4'} transition-all`}>
+    <div className={`absolute flex flex-col items-center mr-0 lg:mr-12 px-12 py-12 lg:py-16 rounded-3xl bg-neutral-900/50 ${active ? '' : 'opacity-0 pointer-events-none select-none translate-y-4'} transition-all`}>
       <h3 className='text-center text-3xl font-bold tracking-wide w-full lg:w-72 pb-2 mb-2 lg:mb-10 border-b-[3px] border-blue-500'>BEJELENTKEZÉS</h3>
 
       <div className="form flex flex-col items-center">
         <Username label='Felhasználónév vagy email' value={username} setValue={setUsername} error={errors.username} setError={(e) => setError('username', e)} validation={false} errors={{ empty: 'Add meg felhasználóneved vagy email címed!' }} />
-        <Password value={password} setValue={setPassword} error={errors.password} setError={(e) => setError('password', e)} reset={props.active} />
+        <Password value={password} setValue={setPassword} error={errors.password} setError={(e) => setError('password', e)} reset={active} />
 
         <Button onClick={Login} text='lg' className='mt-4 lg:mt-16' disabled={Object.values(errors).find(x => !!x)}>Bejelentkezés</Button>
-        <button onClick={() => props.setActive(2)} className='inline lg:hidden mt-2.5 text-sm opacity-60 hover:opacity-75 transition-opacity'>Még nincs felhasználód? Regisztrálj most!</button>
-        <button onClick={() => props.setActive(0)} className='inline lg:hidden mt-0.5 text-sm opacity-60 hover:opacity-75 transition-opacity'>Vissza a főoldalra</button>
+        <button onClick={() => setActive(2)} className='inline lg:hidden mt-2.5 text-sm opacity-60 hover:opacity-75 transition-opacity'>Még nincs felhasználód? Regisztrálj most!</button>
+        <button onClick={() => setActive(0)} className='inline lg:hidden mt-0.5 text-sm opacity-60 hover:opacity-75 transition-opacity'>Vissza a főoldalra</button>
       </div>
 
       {msg === '' ? <></> : <SuccessMsg msg={msg} text={text} />}
