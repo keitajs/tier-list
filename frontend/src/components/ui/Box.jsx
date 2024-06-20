b1756bad5b8061af3380ef67dce96016201e5394
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-20 20:57:54 +0200

    Content box

---
+++ frontend/src/components/ui/Box.jsx
@@ -0,0 +1,7 @@
+export default function Box({ children, className }) {
+  return (
+    <div className={'flex flex-col w-full h-full p-5 rounded-3xl shadow-md shadow-neutral-900/30 bg-neutral-900/50 ' + className}>
+      {children}
+    </div>
+  )
+}
