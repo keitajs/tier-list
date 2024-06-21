867e5541d1c86b7a3a7077c40c0a410e6e5bc304
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-21 20:45:17 +0200

    Input/Box className undefined javítva

--- frontend/src/components/ui/Box.jsx
+++ frontend/src/components/ui/Box.jsx
@@ -1,6 +1,6 @@
 export default function Box({ children, className }) {
   return (
-    <div className={'flex flex-col w-full h-full p-5 rounded-3xl shadow-md shadow-neutral-900/30 bg-neutral-900/50 ' + className}>
+    <div className={'flex flex-col p-5 rounded-3xl shadow-md shadow-neutral-900/30 bg-neutral-900/50 ' + (className ?? '')}>
       {children}
     </div>
   )
--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -17,7 +17,7 @@ export default function Input({ children, label, name, type, value, onChange, me
         {type === 'password' ? <button onClick={() => setShowPass(!showPass)} className='flex items-center px-2'><FontAwesomeIcon icon={showPass ? faEyeSlash : faEye} className='h-3' /></button> : <></>}
       </div>
       <div className='relative'>
-        <input type={type === 'password' ? (showPass ? 'text' : 'password') : type} name={name} id={name} className={`w-64 px-3 pr-10 py-2 bg-neutral-700/75 rounded-lg outline-none ` + className} value={value} onChange={onChange} disabled={disabled} />
+        <input type={type === 'password' ? (showPass ? 'text' : 'password') : type} name={name} id={name} className={`w-64 px-3 pr-10 py-2 bg-neutral-700/75 rounded-lg outline-none ` + (className ?? '')} value={value} onChange={onChange} disabled={disabled} />
         
         <div className='absolute top-1/2 right-3 -translate-y-1/2 flex items-center'>
           <FontAwesomeIcon icon={message ? faXmark : faCheck} className={message ? 'text-rose-500 h-5 input-error-anim' : 'text-emerald-500 h-5 input-check-anim'} />
