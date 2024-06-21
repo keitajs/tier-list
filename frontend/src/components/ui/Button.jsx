b0be66f6fb5c382c179c201e1ca8c3f68edb6f3f
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-21 20:44:37 +0200

    Button padding

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-export default function Button({ children, width, text, color, hover, href, target, onClick, className, disabled }) {
+export default function Button({ children, width, padding, text, color, hover, href, target, onClick, className, disabled }) {
   if (!color || color === 'primary') {
     color = 'bg-blue-500'
     hover = 'hover:bg-blue-400'
@@ -19,13 +19,13 @@ export default function Button({ children, width, text, color, hover, href, targ
   const btnClass = `
     ${width ? `w-${width}` : 'w-full'}
     ${width === 'max' || width === 'min' ? 'px-8' : 'px-0'}
-    py-1.5
+    ${padding === 1 ? 'py-1' : 'py-1.5'}
     ${text ? `text-${text}` : 'text-base'}
     rounded-lg
     ${color || 'bg-blue-500'}
     ${disabled ? 'cursor-not-allowed' : hover || 'hover:bg-blue-400'}
     transition-colors
-    ${className}
+    ${className ?? ''}
   `
 
   return href ? (<Link to={href} target={target} className={btnClass} onClick={onClick}>{children}</Link>) : (<button className={btnClass} onClick={onClick}>{children}</button>)
