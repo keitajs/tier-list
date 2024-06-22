b7d7a6ff4ebc210aad5693a63fa80a998b5f1c2c
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-22 13:42:17 +0200

    Lista szerkesztés kisebb design átalakítások

--- frontend/src/components/List/Lists/ManageList.jsx
+++ frontend/src/components/List/Lists/ManageList.jsx
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faEllipsis, faDumpsterFire, faEye, faEyeSlash, faXmark, faUpRightFromSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { Select, SelectButton } from '../../ui/Select'
 import Button from '../../ui/Button'
+import Input from '../../ui/Input'
+import Textarea from '../../ui/Textarea'
 
 function ManageList({ history, activeList, setActiveList }) {
   const [name, setName] = useState('')
@@ -50,20 +52,18 @@ function ManageList({ history, activeList, setActiveList }) {
   return (
     <>
       <div className='flex justify-between gap-3 mb-5 px-3 pb-2 text-xl border-b-2 border-blue-500'>
-        <div className='relative flex items-center grow'>
-          <input type='text' name="name" id="name" value={name} onChange={e => setName(e.target.value)} placeholder='Lista neve' className='w-full max-w-80 px-2 -mx-2 rounded-lg placeholder:text-neutral-500 bg-neutral-700/50 outline-none resize-none' />
-          {!name ? <div className='absolute top-1/2 right-0 -translate-y-1/2 flex items-center'><FontAwesomeIcon icon={faXmark} className='text-rose-500 h-5 input-error-anim' /></div> : <></>}
-        </div>
+        {activeList ? 'Lista módosítás' : 'Lista létrehozás'}
         <div className={`flex items-center gap-3 text-sm w-max ${activeList ? 'opacity-1 pointer-events-auto' : 'opacity-0 pointer-events-none'} transition-opacity`}>
           <button onClick={() => history(`/list/editor?id=${activeList.id}`)} className='flex items-center gap-2 opacity-75 hover:opacity-90 transition-opacity'><span className='hidden sm:inline'>Megnyitás</span><FontAwesomeIcon icon={faUpRightFromSquare} /></button>
           <button onClick={removeList} className='flex items-center gap-2 group text-rose-600 hover:text-rose-500 ml-0 sm:hover:ml-2 transition-all'><div className='hidden sm:inline sm:w-0 sm:group-hover:w-32 sm:max-w-max overflow-hidden transition-all'>Törlés</div><FontAwesomeIcon icon={faTrash} /></button>
         </div>
       </div>
 
-      <label className='mb-0.5 ml-1.5' htmlFor="description">Leírás</label>
-      <textarea name="description" id="description" rows="2" value={description} onChange={e => setDescription(e.target.value)} className='px-2.5 py-1.5 rounded-lg bg-neutral-700/50 outline-none resize-none mb-3'></textarea>
+      <Input label='Megnevezés' name='name' type='text' maxLength={128} value={name} onChange={e => setName(e.target.value)} message={!name} hideMessage={true} onlyMessage={true} mainClass='!w-full mb-3' labelClass='!text-base' className='!w-full' />
+      
+      <Textarea label='Leírás' name="description" id="description" rows="2" maxLength={256} value={description} onChange={e => setDescription(e.target.value)} hideMessage={true} onlyMessage={true} mainClass='!w-full mb-3' labelClass='!text-base' className='!w-full' />
 
-      <label className='mb-0.5 ml-1.5'>Státusz</label>
+      <label className='mb-0.5 ml-1'>Státusz</label>
       <Select value={status} setValue={setStatus} startIndex={1} className='w-full mb-3'>
         <SelectButton icon={faEllipsis} iconClassName='text-blue-400'>
           Folyamatban
@@ -76,7 +76,7 @@ function ManageList({ history, activeList, setActiveList }) {
         </SelectButton>
       </Select>
 
-      <label className='mb-0.5 ml-1.5'>Láthatóság</label>
+      <label className='mb-0.5 ml-1'>Láthatóság</label>
       <Select value={visible} setValue={setVisible} values={[true, false]} className='w-full mb-3'>
         <SelectButton icon={faEye}>
           Publikus
@@ -88,10 +88,10 @@ function ManageList({ history, activeList, setActiveList }) {
 
       <div className='flex gap-2 justify-center mt-2'>
         {activeList ? <>
-          <Button width='1/3' color='success' onClick={updateList}>Mentés</Button>
+          <Button width='1/3' color='success' onClick={updateList} disabled={!name}>Mentés</Button>
           <Button width='1/3' color='danger' onClick={() => setActiveList(null)}>Mégsem</Button>
         </> :
-          <Button width='1/3' color='success' onClick={createList}>Létrehozás</Button>
+          <Button width='1/3' color='success' onClick={createList} disabled={!name}>Létrehozás</Button>
         }
       </div>
     </>
