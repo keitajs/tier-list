5980e904df143cf114bd238190629c17f41c862f
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-23 19:16:43 +0200

    email.email -> email.address

--- backend/models/emails.js
+++ backend/models/emails.js
@@ -3,7 +3,7 @@ import { db } from '../libs/database.js'
 
 const emails = db.define('emails', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  email: { type: DataTypes.STRING(128), allowNull: false },
+  address: { type: DataTypes.STRING(128), allowNull: false },
   code: { type: DataTypes.STRING(6), allowNull: true },
   expDate: { type: DataTypes.DATE, allowNull: false }
 }, { createdAt: false, updatedAt: false });
