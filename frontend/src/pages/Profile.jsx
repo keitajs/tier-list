a7f52ade4d923b0f11266cfbbdd5e45ceeccb38a
keitajs <126980311+keitajs@users.noreply.github.com>
2024-06-30 19:40:18 +0200

    Profil oldal átdolgozás elkezdve

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import Username from '../components/Profile/Edit/Username'
 import Avatar from '../components/Profile/Edit/Avatar'
 import Email from '../components/Profile/Edit/Email'
 import Password from '../components/Profile/Edit/Password'
+import Box from '../components/ui/Box'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
 
 function Profile({ history }) {
   const params = useParams()
@@ -47,7 +50,58 @@ function Profile({ history }) {
     <div className='min-h-screen p-6'>
       <Navigation history={history} loading={!user.username} />
 
-      <div className='h-full ml-0 sm:ml-14 mb-16 sm:mb-0'>
+      <div className='flex flex-col-reverse md:flex-row gap-6 h-full ml-0 sm:ml-14 mb-16 sm:mb-0'>
+        <div className='flex flex-wrap gap-6 w-full md:w-1/2 xl:w-2/3 *:!w-1/2 *:xl:!w-1/3 *:!h-auto *:aspect-video *:grow'>
+          <Box>
+            {/* <Activity weeklyActivies={weeklyActivies} /> */}
+          </Box>
+          <Box>
+            {/* <Characters characters={list?.characters?.mostUsed || []} /> */}
+          </Box>
+          <Box>
+            {/* <Lists lists={list?.mostUpdated || []} params={hasUser} history={history} /> */}
+          </Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+        </div>
+        <div className='w-full md:w-1/2 xl:w-1/3'>
+          <Box className='group !relative !flex !flex-row !gap-3 !p-2.5'>
+            <button
+              onClick={!hasUser ? () => setEdit('avatar') : undefined}
+              className={`flex items-center justify-center w-36 h-36 rounded-2xl overflow-hidden border-2 border-transparent ${!hasUser ? 'hover:border-neutral-600' : 'cursor-default'} transition-colors`}
+            >
+              {user.avatar ?
+                <img
+                  src={`${axios.defaults.baseURL}/user/images/${user.avatar}?autoRefresh=true&refreshKey=${Math.floor(Math.random() * 10**8)}`}
+                  alt="Profilkép"
+                  className='w-full h-full object-cover'
+                />
+              :
+                <div className='w-1/4 h-1/4 rounded-full border-e-4 border-neutral-700/50 animate-spin'></div>
+              }
+            </button>
+            <div>
+              <div className='flex flex-col'>
+                <div className='flex items-center gap-2 text-2xl'>
+                  {user.username}
+                  {!hasUser &&
+                    <FontAwesomeIcon
+                      icon={faEdit}
+                      onClick={() => setEdit('username')}
+                      className='cursor-pointer h-3.5 opacity-0 group-hover:opacity-25 hover:!opacity-65 transition-opacity'
+                    />
+                  }
+                </div>
+                <div className='text-sm opacity-50'>Admin</div>
+              </div>
+            </div>
+            <div className={`absolute right-5 top-5 w-3 h-3 rounded-full ${user.status === 1 ? 'bg-emerald-500' : 'bg-rose-500'}`}></div>
+          </Box>
+        </div>
+      </div>
+
+      {/* <div className='h-full ml-0 sm:ml-14 mb-16 sm:mb-0'>
         <div className='flex flex-col gap-6 relative lg:fixed lg:right-6 lg:top-6 lg:bottom-6 w-full lg:w-1/5 mb-3 lg:mb-0'>
           <div className='flex items-center justify-center group relative max-w-96 w-full mx-auto aspect-square rounded-3xl bg-neutral-900/85 overflow-hidden'>
             {user.avatar ? <img src={`${axios.defaults.baseURL}/user/images/${user.avatar}?autoRefresh=true&refreshKey=${Math.floor(Math.random() * 10**8)}`} alt="" className='w-full h-full object-cover' /> : <div className='w-1/4 h-1/4 rounded-full border-e-4 border-neutral-700/50 animate-spin'></div>}
@@ -60,7 +114,7 @@ function Profile({ history }) {
           <Characters characters={list?.characters?.mostUsed || []} />
           <Lists lists={list?.mostUpdated || []} params={hasUser} history={history} />
         </div>
-      </div>
+      </div> */}
 
       <Username hide={edit !== 'username'} currentUsername={user.username} setUser={setUser} setEdit={setEdit} />
       <Email hide={edit !== 'email'} currentEmail={user.email} setUser={setUser} setEdit={setEdit} />
